b2fffc9433567c4d9cf5fcf526121a7de0708ddc
AndyBoltonMV <andrew.bolton@multiverse.io>
2023-02-03 12:46:10 +0000

    ref

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
-const paragraph = document.querySelector("#demo");
-const button = document.querySelector("#bttn");
+const main = document.querySelector("main");
 
-paragraph.innerHTML = "Gary Steve";
+const arr = ["Steve", "Gary", "Clive", "Susan", "Toby"];
 
-function changeColor() {
-  console.log("Green!");
+main.innerHTML += `
+  <section class="image-name-container">
+    <p>Clive Clive</p>
+  </section>
+`;
+
+function nodeCreation(name) {
+  const section = document.createElement("section");
+  section.className = "image-name-container";
+  const p = document.createElement("p");
+  const textNode = document.createTextNode(name);
+
+  p.appendChild(textNode);
+  section.appendChild(p);
+  main.appendChild(section);
 }
 
-button.addEventListener("click", changeColor);
+for (let i = 0; i < arr.length; i++) {
+  nodeCreation(arr[i]);
+}
+
+console.log(main.childNodes);
